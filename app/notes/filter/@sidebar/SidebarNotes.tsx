4a1693dbde80d0cd016907e730cf339832dd462c
Drapak Dmytro <dd.study05@gmail.com>
2026-06-07 09:38:36 +0300

    0.8-0.8

--- app/notes/filter/@sidebar/SidebarNotes.tsx
+++ app/notes/filter/@sidebar/SidebarNotes.tsx
@@ -1,25 +1,32 @@
 import Link from "next/link";
 import css from "./SidebarNotes.module.css";
 
-const TAGS = [
-  "Todo",
-  "Work",
-  "Personal",
-  "Meeting",
-  "Shopping",
-];
+interface SidebarNotesProps {
+  tags: string[];
+}
 
-export default function SidebarNotes() {
+export default function SidebarNotes({
+  tags,
+}: SidebarNotesProps) {
   return (
     <ul className={css.menuList}>
       <li className={css.menuItem}>
-        <Link href="/notes/filter/all" className={css.menuLink}>
+        <Link
+          href="/notes/filter/all"
+          className={css.menuLink}
+        >
           All notes
         </Link>
       </li>
-      {TAGS.map((tag) => (
-        <li key={tag} className={css.menuItem}>
-          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+      {tags.map((tag) => (
+        <li
+          key={tag}
+          className={css.menuItem}
+        >
+          <Link
+            href={`/notes/filter/${tag}`}
+            className={css.menuLink}
+          >
             {tag}
           </Link>
         </li>
--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import css from "./SidebarNotes.module.css";
+import SidebarNotes from "./SidebarNotes";
 
 const TAGS = [
   "Todo",
@@ -10,29 +9,5 @@ const TAGS = [
 ];
 
 export default function SidebarDefault() {
-  return (
-    <ul className={css.menuList}>
-      <li className={css.menuItem}>
-        <Link
-          href="/notes/filter/all"
-          className={css.menuLink}
-        >
-          All notes
-        </Link>
-      </li>
-      {TAGS.map((tag) => (
-        <li
-          key={tag}
-          className={css.menuItem}
-        >
-          <Link
-            href={`/notes/filter/${tag}`}
-            className={css.menuLink}
-          >
-            {tag}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+  return <SidebarNotes tags={TAGS} />;
 }
